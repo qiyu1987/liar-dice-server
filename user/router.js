9624e39175ebfd0c6b8b480ce57453e79d3a7dc0
qiyu1987 <qiyu1987@gmail.com>
2019-12-30 14:35:30 +0100

    user factory

--- user/router.js
+++ user/router.js
@@ -1,13 +1,15 @@
 const { Router } = require("express")
-const router = new Router()
 const User = require("./model")
-
-router.post("/user", async (req, res, next) => {
-	try {
-		const user = await User.create(req.body)
-		res.send(user)
-	} catch (err) {
-		next(err)
-	}
-})
-module.exports = router
+function factory(stream) {
+	const router = new Router()
+	router.post("/user", async (req, res, next) => {
+		try {
+			const user = await User.create(req.body)
+			res.send(user)
+		} catch (err) {
+			next(err)
+		}
+	})
+	return router
+}
+module.exports = factory
